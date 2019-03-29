263d1458a3ace1b3af28a6b94cd63f29a7a017bc
Michał O <micavanco@gmai.com>
2019-03-29 20:46:46 +0100

    Added placeholder handler to column textfield

--- src/resources/Resources.java
+++ src/resources/Resources.java
@@ -1,4 +1,31 @@
 package resources;
 
+import javafx.scene.control.TextField;
+
 public class Resources {
+    public TextField columnField;
+    public boolean onLoad;
+
+    public void clickedColumnLabel()
+    {
+        if(columnField.getText().equals("Write name of column"))
+            columnField.setText("");
+
+        if(onLoad)
+        {
+            columnField.focusedProperty().addListener((obs, oldV, newV)->{
+                if(columnField.getText().isEmpty())
+                    columnField.setText("Write name of column");
+
+            });
+            onLoad = false;
+        }
+
+    }
+
+    public Resources()
+    {
+        onLoad = true;
+    }
+
 }
