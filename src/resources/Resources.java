8aad94b7f1c93d8f32072698a3c262dc38a4f07d
Michał O <micavanco@gmai.com>
2019-03-29 21:41:46 +0100

    Added add column handler and remove column handler with the whole logic

--- src/resources/Resources.java
+++ src/resources/Resources.java
@@ -1,10 +1,18 @@
 package resources;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Resources {
     public TextField columnField;
+    public TextField query;
     public boolean onLoad;
+    public Label queryLabel;
+    public String tabel;
+    public List<String> columns;
 
     public void clickedColumnLabel()
     {
@@ -26,6 +34,44 @@ public class Resources {
     public Resources()
     {
         onLoad = true;
+        columns = new ArrayList<String>();
+    }
+
+    public void typingTableName()
+    {
+        tabel = query.getText();
+        setQuery();
+    }
+
+    public void onAddColumn()
+    {
+        columns.add("'"+columnField.getText()+"'");
+        setQuery();
+        columnField.setText("");
+    }
+
+    public void onRemoveColumn()
+    {
+        String temp = "'"+columnField.getText()+"'";
+        if(columns.contains(temp))
+        {
+            columns.remove(temp);
+            setQuery();
+            columnField.setText("");
+        }
+    }
+
+    public void setQuery()
+    {
+        String columnsString = "(";
+        if(!columns.isEmpty())
+        {
+            for(String s: columns)
+                columnsString+=(s+", ");
+            columnsString=columnsString.substring(0, columnsString.length()-2);
+        }
+
+        queryLabel.setText("insert from "+tabel+" "+columnsString+")");
     }
 
 }
